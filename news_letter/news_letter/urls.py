893f16fa13865f36c497ef047411d7f263ad77f9
chrisshortlaw <chris@chrisshortlaw.com>
2021-12-08 12:58:46 +0000

    Added home_page app, url

--- news_letter/news_letter/urls.py
+++ news_letter/news_letter/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('index/', include('home_page.urls'))
 ]
