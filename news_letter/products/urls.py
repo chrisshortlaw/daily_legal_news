1f977bcd3432ab07061babd0b6e28a246d553398
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-16 17:18:41 +0000

    Fixed broken url

--- news_letter/products/urls.py
+++ news_letter/products/urls.py
@@ -7,7 +7,7 @@ from . import views
     name = [insert name found in Config class in apps.py]
 '''
 urlpatterns = [
-        path('', views.all_products, name='products'),
+        path('products/', views.all_products, name='products'),
         path('products/<product_name>/',
              views.product_page,
              name='product_page')
