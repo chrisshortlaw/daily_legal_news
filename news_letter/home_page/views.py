c1e89423799a0d594fd1028d482632cc42159782
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-16 17:51:02 +0000

    Updated views, increased context variables

--- news_letter/home_page/views.py
+++ news_letter/home_page/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from article_page.models import Author, Article, Comment
+from products.models import Category, Product
 # Create your views here.
 
 
 def index(request):
-    return render(request=request, template_name='home_page/index.htmldjango')
+
+    front_page_articles = Article.objects.all()
+
+    head_articles = Article.objects.order_by('date')[:5]
+    art_authors = [article.author.all() for article in front_page_articles]
+    head_liner = head_articles[0]
+    head_liner_author = head_liner.author.all()
+    head_liner_article = {"head_liner": head_liner,
+                          "head_liner_title": head_liner.title,
+                          "head_liner_id": head_liner.id,
+                          "head_liner_author_name": head_liner_author[0].name,
+                          "head_liner_author_id": head_liner_author[0].id}
+    top_articles = []
+    for article in head_articles[1:]:
+        top_art_dict = {}
+        top_art_dict["title"] = article.title
+        top_art_dict["id"] = article.id
+        art_author = article.author.all()
+        top_art_dict["author"] = []
+        for author in art_author:
+            author_dict = {}
+            author_dict["id"] = author.id
+            author_dict["name"] = author.name
+            top_art_dict["author"].append(author_dict)
+        top_articles.append(top_art_dict)
+
+    products = Product.objects.all()
+
+    context = {
+            "head_liner_article": head_liner_article,
+            "top_articles": top_articles,
+            "Articles": front_page_articles,
+            "Authors": art_authors,
+            "Products": products
+            }
+
+    return render(request=request,
+                  template_name='home_page/index.htmldjango',
+                  context=context)
