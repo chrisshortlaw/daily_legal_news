d453695ee992aba6aa28fcd64207b6c94a5b7e92
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-17 13:25:39 +0000

    Added cart functionality

--- news_letter/cart/urls.py
+++ news_letter/cart/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         path('', views.cart, name='cart'),
+        path('add/<product_id>/', views.add_to_cart, name="add_to_cart")
         ]
--- news_letter/cart/views.py
+++ news_letter/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,3 +6,22 @@ from django.shortcuts import render
 def cart(request):
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, product_id):
+    '''
+    Adds a product to a user's cart
+    '''
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if product_id in list(cart.keys()):
+        cart[product_id] += quantity
+    else:
+        cart[product_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+
+    return redirect(redirect_url)
