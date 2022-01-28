1b51a2a25564738d23ee8142ac70ab4f5fb72564
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-28 20:34:06 +0000

    Updated urls

---
+++ news_letter/subscriptions/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+        path('', views.subscriptions, name='subscriptions'),
+        path('subscribe/',
+             views.create_checkout_session,
+             name='subscription_checkout'),
+        path('subscribe/success',
+             views.subscription_success,
+             name="subscription_success"),
+        path('subscribe/fail',
+             views.subscription_fail,
+             name="subscription_fail")
+        ]
