cfe040829b816ab98f92413017df1deae946ac00
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-29 21:11:53 +0000

    Added id to Product Display

--- news_letter/products/admin.py
+++ news_letter/products/admin.py
@@ -6,6 +6,7 @@ from .models import Product, Category
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
+            "id",
             "category",
             "name",
             "description",
