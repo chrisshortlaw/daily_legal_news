4cd1d900b44a4684f88c024eb5ca8c5c39c05b2f
chrisshortlaw <chris@chrisshortlaw.com>
2022-01-29 21:03:59 +0000

    Added checkout & subscribe javascript

--- news_letter/checkout/static/js/checkout.js
+++ news_letter/checkout/static/js/checkout.js
@@ -58,7 +58,7 @@ form.addEventListener('submit', function(ev) {
     let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
     
     let postData = { 
-      'csrfmiddlewaretoken': csrftoken,
+      'csrfmiddlewaretoken': csrfToken,
       'client_secret': clientSecret,
       'save_info': saveInfo,
     };
