f22603d37feaa2cc08f6d52f1f22e69f10a1bf6b
chrisshortlaw <chris@chrisshortlaw.com>
2022-02-05 22:46:12 +0000

    Updated sections, views

--- home_page/views.py
+++ home_page/views.py
@@ -9,6 +9,74 @@ def index(request):
 
     front_page_articles = Article.objects.all()
 
+    head_articles = Article.objects.order_by('date')[:4]
+    art_authors = [article.author.all() for article in front_page_articles]
+    head_liner = head_articles[0]
+    head_liner_author = head_liner.author.all()
+    head_liner_article = {
+                          "head_liner": head_liner,
+                          "head_liner_title": head_liner.title,
+                          "head_liner_id": head_liner.id,
+                          "head_liner_author_name": head_liner_author[0].name,
+                          "head_liner_author_id": head_liner_author[0].id,
+                          "head_liner_author_slug": head_liner_author[0].slug,
+                          "head_liner_image_url": head_liner.article_image_url,
+                          "head_liner_comment_count": head_liner.comment_count,
+                          "date": head_liner.date
+                          }
+
+    top_articles = []
+    for article in head_articles[1:]:
+        top_art_dict = {}
+        top_art_dict["title"] = article.title
+        top_art_dict["id"] = article.id
+        top_art_dict["top_art_date"] = article.date
+        top_art_dict['comment_count'] = article.comment_count
+        art_author = article.author.all()
+        top_art_dict["author"] = []
+        for author in art_author:
+            author_dict = {}
+            author_dict["id"] = author.id
+            author_dict["name"] = author.name
+            author_dict["author_slug"] = author.slug
+            top_art_dict["author"].append(author_dict)
+        top_articles.append(top_art_dict)
+
+    block_articles = []
+    for article in front_page_articles[1:]:
+        block_art_dict = {}
+        block_art_dict["title"] = article.title
+        block_art_dict["id"] = article.id
+        block_art_dict["top_art_date"] = article.date
+        block_art_dict['comment_count'] = article.comment_count
+        block_art_dict['image_url'] = article.article_image_url
+        block_art_dict['teaser'] = article.teaser
+        art_author = article.author.all()
+        block_art_dict["author"] = []
+        for author in art_author:
+            author_dict = {}
+            author_dict["id"] = author.id
+            author_dict["name"] = author.name
+            author_dict["author_slug"] = author.slug
+            block_art_dict["author"].append(author_dict)
+        block_articles.append(block_art_dict)
+
+    context = {
+            "head_liner_article": head_liner_article,
+            "top_articles": top_articles,
+            "Articles": front_page_articles,
+            "Authors": art_authors,
+            "block_articles": block_articles
+            }
+
+    return render(request=request,
+                  template_name='home_page/index.htmldjango',
+                  context=context)
+
+def headline(request):
+
+    front_page_articles = Article.objects.all()
+
     head_articles = Article.objects.order_by('date')[:5]
     art_authors = [article.author.all() for article in front_page_articles]
     head_liner = head_articles[0]
@@ -38,16 +106,14 @@ def index(request):
             top_art_dict["author"].append(author_dict)
         top_articles.append(top_art_dict)
 
-    products = Product.objects.all()
-
     context = {
             "head_liner_article": head_liner_article,
             "top_articles": top_articles,
             "Articles": front_page_articles,
             "Authors": art_authors,
-            "Products": products
             }
 
     return render(request=request,
-                  template_name='home_page/index.htmldjango',
+                  template_name='home_page/alt_index.htmldjango',
                   context=context)
+
